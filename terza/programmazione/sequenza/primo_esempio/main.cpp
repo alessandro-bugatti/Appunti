cd9f9e6bff8422cd41b48cfc9f111c9103ae9e3a
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-10-08 09:13:36 +0200

    Aggiunto il primo esempio di programma con I/O

---
+++ terza/programmazione/sequenza/primo_esempio/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Inserisci un numero intero: ";
+    cin >> N; //input
+    N = N + 1;
+    cout << "Il numero che hai inserito è: " << N << endl; //output
+    return 0;
+}
