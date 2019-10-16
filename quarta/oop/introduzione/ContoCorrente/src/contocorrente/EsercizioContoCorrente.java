8a480918fd81371bc0ea93214a9ed8ec75ff18e3
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-10-16 12:26:38 +0200

    Aggiunge il primo esempio di classe appena abbozzato

---
+++ quarta/oop/introduzione/ContoCorrente/src/contocorrente/EsercizioContoCorrente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contocorrente;
+
+/**
+ *
+ * @author Alessandro Bugatti <alessandro.bugatti@gmail.com>
+ */
+public class EsercizioContoCorrente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ContoCorrente c;
+        c = new ContoCorrente();
+        c.Nominativo = "Geppo Gepponi";
+    }
+    
+}
