fcb3a4fd08a383be67fc669bc239e0b155ebb488
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-10-17 17:21:17 +0200

    Aggiunge il primo esempio di programma con I/O

---
+++ universita/programmazione_c/programmi_semplici/somma/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Inserisci un numero intero: ");
+    scanf("%d",&a);
+    printf("Inserisci un numero intero: ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("La somma vale %d \n", c);
+
+    return 0;
+}
