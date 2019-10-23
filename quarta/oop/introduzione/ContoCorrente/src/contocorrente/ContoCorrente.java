f8f1b7452aac89e8a56dc073bf27e66f42f65ddf
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-10-23 17:39:04 +0200

    Aggiorna con le modifiche proposte da Usanza

--- quarta/oop/introduzione/ContoCorrente/src/contocorrente/ContoCorrente.java
+++ quarta/oop/introduzione/ContoCorrente/src/contocorrente/ContoCorrente.java
@@ -1,77 +1,71 @@
 /*
-  Copyright (C) 2013 Alessandro Bugatti (alessandro.bugatti@istruzione.it)
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-/*! \file
- *  \brief 
- *  \author Alessandro Bugatti
- *  \version 0.1
- *  \date  Creazione 16-ott-2019
- *  \date  Ultima modifica 16-ott-2019
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package contocorrente;
 
-
+/**
+ *
+ * @author Usanza Stefano
+ */
 public class ContoCorrente {
-    private String nominativo;
-    private int codice;
-    private float saldo;
+    private String nominativo; //private viene visto solo all'interno della classe corrente
+    private int codice; 
+    private float soldiPresenti;
     private EstrattoConto estratto;
-
-    /*
-    public ContoCorrente(){
-        this.nominativo = "No nome";
-        codice = -1;
-        saldo = 0;
-    }
-    */
-    public ContoCorrente(String nominativo){
+    
+/*    public ContoCorrente(){//costruttore
+        this.nominativo= "no nome";
+        this.codice= -1;
+        this.soldiPresenti=0;
+    }*/
+    
+    public ContoCorrente(String nominativo){//non deve essere per forza unico
         this.nominativo = nominativo;
-        this.codice = (int)(Math.random()*1000000);
+        this.codice = (int)(Math.random()*100000000);
         this.estratto = new EstrattoConto();
     }
     
-    public void deposita(float soldi){
-        if (soldi > 10000){
-            estratto.aggiungiOperazione(soldi, "", false);
+    public void deposita(float n){
+        if (n > 10000){
+            estratto.aggiungiOperazione(n, "deposito", false);
             return;
         }
-        this.saldo += soldi;
-        estratto.aggiungiOperazione(soldi, "", true);
+        this.soldiPresenti += n;
+        estratto.aggiungiOperazione(n, "deposito", true);
     }
     
-    public void preleva(float soldi){
-        if (this.saldo - soldi < 0)
+    private void deposita(String soldi){
+        float n= Float.valueOf(soldi);
+        deposita(n);
+    }
+    
+    public void preleva(float n){
+        if (this.soldiPresenti < n){
+            estratto.aggiungiOperazione(n, "prelievo", false);
             return;
-        this.saldo -= soldi;
+        }
+        this.soldiPresenti -= n;
+        estratto.aggiungiOperazione(n, "prelievo", true);        
     }
     
-    public float getSaldo()
-    {
-        return saldo;
+    public void preleva(String soldi){
+        float n= Float.valueOf(soldi);
+        preleva(n);
+        
     }
     
-    public String toString()
-    {
-        String s = "Codice conto:" + codice + ". Questo conto corrente è di " + this.nominativo
-                + ". Il saldo è " + this.saldo + "\n";
-        s += estratto;
-        return s;
+    public float getSaldo(){
+        return this.soldiPresenti;
     }
     
+    @Override
+    public String toString(){
+       String s = "conto corrente di " + this.nominativo
+                  + " codice " +  this.codice + " il saldo è " + this.soldiPresenti + "\n";
+       s += estratto;
+       return s;
+    }
+
 }
