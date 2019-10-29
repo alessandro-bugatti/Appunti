651796a3be3457ed264a35560182da0717880103
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-10-29 12:27:55 +0100

    Aggiunge l'esercizio del craps

---
+++ terza/programmazione/iterazione/craps/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    Gioco del craps
+    Vengono lanciati due dadi da 6:
+    - se esce 7 o 11 il giocatore vince
+    - se esce 2, 3 o 12 il giocatore perde
+    - se esce un altro numero, questo è il target
+    Si continua a lanciare finchè
+    - Esce il target allora il giocatore vince
+    - Esce 7 allora il giocatore perde
+    - Altrimenti si lancia ancora
+*/
+
+
+
+int main()
+{
+
+    return 0;
+}
