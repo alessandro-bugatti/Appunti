973b1d0e73d87763b9c241867e90a711a18e0483
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-12-03 11:58:07 +0100

    Migliora il codice richiamando la funzione per definirne un'altra

--- terza/programmazione/funzioni/overloading/main.cpp
+++ terza/programmazione/funzioni/overloading/main.cpp
@@ -2,12 +2,7 @@
 
 using namespace std;
 
-//Funzione che ritorna n numeri interi
-//compresi tra 1 e n
-int random(int n)
-{
-    return rand()%n + 1;
-}
+
 
 //Funzione che ritorna n numeri interi
 //compresi tra inizio e fine estremi inclusi
@@ -16,8 +11,17 @@ int random(int inizio, int fine)
     return rand()%(fine - inizio + 1) + inizio;
 }
 
+//Funzione che ritorna n numeri interi
+//compresi tra 1 e n
+int random(int n)
+{
+    return random(1,n);
+}
+
 int main()
 {
     cout << random(3,6) << endl;
+    for (int i = 0; i < 100; i++)
+        cout << random(6) << endl;
     return 0;
 }
