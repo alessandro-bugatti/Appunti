ed6827e6af979801da2250ef867c2944b07b7d2c
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-01-25 16:29:06 +0100

    Migliora e completa l'aggiornamento di un record

--- quinta/htdocs/es_16/aggiorna_libro.php
+++ quinta/htdocs/es_16/aggiorna_libro.php
@@ -7,8 +7,39 @@
 <body>
     <pre>
     <?php
-        var_dump($_POST);
-        var_dump($_COOKIE);
+        require_once('config.php');
+        //var_dump($_POST);
+        //var_dump($_COOKIE);
+        $ISBN = $_POST['ISBN'];
+        $titolo = htmlspecialchars($_POST['titolo']);
+        $lingua = $_POST['lingua'];
+        $anno = $_POST['anno'];
+        $prezzo = $_POST['prezzo'];
+        $id_editore = $_POST['id_editore'];
+        $id = $_COOKIE['id'];
+        $sql = 'UPDATE libro SET ISBN =:ISBN, titolo = :titolo, lingua =:lingua, anno = :anno, prezzo = :prezzo, id_editore = :id_editore WHERE id = :id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(
+            [
+                'ISBN' => $ISBN,
+                'titolo' => $titolo,
+                'lingua' => $lingua,
+                'anno' => $anno,
+                'prezzo' => $prezzo,
+                'id_editore' => $id_editore,
+                'id' => $id
+            ]
+        );
+        //Altra possibilità per chiudere l'operazione
+        //Ridirezione verso una pagina che mostra
+        //l'elenco
+        //header("Location: /es_16/lista_libri.php");
+
+        if ($stmt->rowCount() == 1)
+            echo '<p>Il libro è stato modificato con successo </p>';
+        else {
+            echo '<p>Il libro non è stato modificato, contatta l\'amministratore</p>';
+        }
      ?>
      </pre>
 </body>
--- quinta/htdocs/es_16/modifica.php
+++ quinta/htdocs/es_16/modifica.php
@@ -1,3 +1,7 @@
+<?php
+    $id = $_GET['id'];
+    setcookie("id", $id);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +10,35 @@
 </head>
 <?php
     require_once('config.php');
+
+    function scegli_anno($anno)
+    {
+        $selected = "";
+        for ($i = 1900; $i <= 2020; $i++)
+        {
+            if ($i == $anno)
+                $selected = "selected";
+            echo "<option value=\"" .$i ."\" " . $selected . ">" . $i . "</option>\n";
+            $selected = "";
+        }
+    }
+
+    function scegli_editore($pdo, $id_editore)
+    {
+        $stmt = $pdo->query('SELECT ragione_sociale, id FROM editore ORDER BY ragione_sociale');
+        $selected = "";
+        while ($row = $stmt->fetch())
+        {
+            if ($row['id'] == $id_editore)
+                $selected = "selected";
+            echo '<option value="' . $row['id']. '"'. $selected . '>' . $row['ragione_sociale'] . '</option>';
+            $selected = "";
+        }
+    }
 ?>
 <body>
     <h1>Modifica libro</h1>
     <?php
-        $id = $_GET['id'];
-        setcookie("id", $id);
         $sql = 'SELECT * FROM libro WHERE id = :id';
         $stmt = $pdo->prepare($sql);
         $stmt->execute(
@@ -21,14 +48,24 @@
         $ISBN = $row['ISBN'];
         $titolo = $row['titolo'];
         $lingua = $row['lingua'];
+        $anno = $row['anno'];
+        $prezzo = $row['prezzo'];
+        $id_editore = $row['id_editore'];
     ?>
     <form action="aggiorna_libro.php" method="post">
     <!-- Ci si limita ai primi tre campi per brevità -->
         ISBN <input type="text" name="ISBN" value="<?php echo $ISBN ?>"><br>
         Titolo <input type="text" name="titolo" value="<?php echo $titolo ?>"> <br>
         Lingua <input type="text" name="lingua" value="<?php echo $lingua ?>"><br>
-        <input type="hidden" name="id" value="<?php echo $id ?>">
-
+        Anno <select name = "anno">
+        <?php
+            scegli_anno($anno);
+        ?>
+    </select><br>
+        Prezzo <input type="text" name="prezzo" value="<?php echo $prezzo ?>"><br>
+    Editore <select name="id_editore">
+        <?php scegli_editore($pdo, $id_editore); ?>
+    </select><br>
     <input type="submit" value="Modifica i dati del libro">
     </form>
 </body>
