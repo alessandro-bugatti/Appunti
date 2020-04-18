52380297c2cda3adffd92bf034e5df0422195ac1
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-04-18 19:37:10 +0200

    Aggiunge delle funzioni con strutture

--- terza/programmazione/struttura/frazione/main.cpp
+++ terza/programmazione/struttura/frazione/main.cpp
@@ -7,6 +7,33 @@ struct Frazione{
     int denominatore;
 };
 
+void stampa(Frazione f)
+{
+    cout << endl << f.numeratore << "/"
+    << f.denominatore << endl;
+}
+
+void input(Frazione &f)
+{
+    cout << "Inserisci il numeratore: ";
+    cin >> f.numeratore;
+    cout << "Inserisci il denominatore: ";
+    cin >> f.denominatore;
+}
+
+//Prima versione, in cui non si riduce la frazione
+//ai minimi termini
+Frazione somma(Frazione a, Frazione b)
+{
+    Frazione c;
+    c.numeratore = a.numeratore*b.denominatore
+        + b.numeratore*a.denominatore;
+    c.denominatore = a.denominatore * b.denominatore;
+    return c;
+}
+
+
+
 int main()
 {
     Frazione a;
@@ -17,11 +44,14 @@ int main()
     b = a;
     Frazione c;
     cout << "Inserisci una frazione" << endl;
+
     //cin >> c; NO
     cout << "Inserisci il numeratore: ";
     cin >> c.numeratore;
     cout << "Inserisci il denominatore: ";
     cin >> c.denominatore;
+    //Oppure di può usare la funzione input(c);
+
     //cout << c; NO
     cout << endl << c.numeratore << "/" << c.denominatore << endl;
     cout << endl << c.numeratore << endl << "-" << endl << c.denominatore << endl;
@@ -38,5 +68,18 @@ int main()
     ----
     353
     */
+    /**
+    Operatori di confronto, si possono usare?
+    == > < >= <= !=
+    //if ( a <= b ) // NO, nessuno di quelli che contengono < o >
+    //if ( a == b) // SI, fanno un confronto byte a byte
+    Operatori aritmetici, si possono usare?
+    // +=/* NO
+    */
+    //Esempio di utilizzo di funzioni con strutture
+    input(a);
+    input(b);
+    c = somma(a,b);
+    stampa(c);
     return 0;
 }
