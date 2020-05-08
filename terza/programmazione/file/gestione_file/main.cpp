47c0bb0f55d6eb55256d7437df05dcf02eb3df25
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-05-08 12:34:24 +0200

    Primo esempio di gestione dei file, da completare

---
+++ terza/programmazione/file/gestione_file/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+    Per tutti i file abbiamo almeno tre operazioni:
+    1) Apertura
+    2) Lettura/scrittura
+    3) Chiusura
+*/
+
+int main()
+{
+    ifstream in("prova.txt"); //Apro un file di input
+
+    return 0;
+}
