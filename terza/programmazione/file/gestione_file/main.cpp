5a86dc0434cec53921d841bdc4cf19e3db8ba694
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-05-09 10:01:47 +0200

    Aggiorna l'esempio sui file

--- terza/programmazione/file/gestione_file/main.cpp
+++ terza/programmazione/file/gestione_file/main.cpp
@@ -14,5 +14,20 @@ int main()
 {
     ifstream in("prova.txt"); //Apro un file di input
 
+    //Controllo sull'apertura
+    if (!in)
+    {
+        //Reazione all'errore
+       cout << "Errore nell'apertura del file" << endl;
+       return 0;
+    }
+    //Fase di lettura
+    int n, m;
+    in >> n ;
+    for (int i = 0; i < n; i++)
+    {
+        in >> m;
+        cout << m << endl;
+    }
     return 0;
 }
