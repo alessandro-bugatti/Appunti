5537532d67ad2a54f3914236878486e594fb2971
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-05-11 15:03:12 +0200

    Aggiunge esempi sui file

--- terza/programmazione/file/gestione_file/main.cpp
+++ terza/programmazione/file/gestione_file/main.cpp
@@ -12,7 +12,8 @@ using namespace std;
 
 int main()
 {
-    ifstream in("prova.txt"); //Apro un file di input
+    //Gestione file con contatore
+    ifstream in("contatore.txt"); //Apro un file di input
 
     //Controllo sull'apertura
     if (!in)
@@ -21,13 +22,75 @@ int main()
        cout << "Errore nell'apertura del file" << endl;
        return 0;
     }
-    //Fase di lettura
+    //Fase di lettura modalità con contatore
     int n, m;
+    //Leggo il contatore all'inizio del file
+    cout << "Modalita' con contatore" << endl;
     in >> n ;
+    //Leggo gli n numeri
     for (int i = 0; i < n; i++)
     {
         in >> m;
         cout << m << endl;
     }
+    in.close();//Chiude il file
+
+    //Gestione file fino alla fine
+    ifstream in2("completo.txt"); //Apro un file di input
+
+    //Controllo sull'apertura
+    if (!in2)
+    {
+        //Reazione all'errore
+       cout << "Errore nell'apertura del file" << endl;
+       return 0;
+    }
+    //Fase di lettura modalità fino alla fine
+
+    cout << "Modalita' fino alla fine" << endl;
+
+    //Leggo tutto il contenuto
+    while(in2 >> m)
+        cout << m << endl;
+
+
+    //Gestione file fino alla fine con valori di tipo diverso
+    ifstream in3("interi_float.txt"); //Apro un file di input
+
+    //Controllo sull'apertura
+    if (!in3)
+    {
+        //Reazione all'errore
+       cout << "Errore nell'apertura del file" << endl;
+       return 0;
+    }
+    //Fase di lettura modalità fino alla fine
+
+    cout << "Modalita' fino alla fine con interi e reali" << endl;
+    int intero;
+    float reale;
+    //Leggo tutto il contenuto
+    while(in3 >> intero >> reale)
+        cout << intero << " " << reale << endl;
+
+    //Gestione file fino alla fine senza struttura
+    ifstream in4("senza_struttura.txt"); //Apro un file di input
+
+    //Controllo sull'apertura
+    if (!in4)
+    {
+        //Reazione all'errore
+       cout << "Errore nell'apertura del file" << endl;
+       return 0;
+    }
+    //Fase di lettura modalità fino alla fine
+
+    cout << "Modalita' fino alla fine senza struttura interna" << endl;
+    string s;
+    //Leggo tutto il contenuto
+    while(in4 >> s)
+        cout << s << endl;
+
+
     return 0;
 }
