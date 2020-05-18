ebac2bcf68ffd570d738d6faeca3765a04043471
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-05-18 15:46:02 +0200

    Primo esempio di utilizzo della libreria Gluon, l'oroscopo

---
+++ quarta/gluon/Oroscopo/OroscopoApp/src/main/java/com/oroscopo/Oroscopo.java
@@ -0,0 +1,31 @@
+package com.oroscopo;
+
+import com.oroscopo.views.PrimaryView;
+import com.oroscopo.views.SecondaryView;
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Oroscopo extends MobileApplication {
+
+    public static final String PRIMARY_VIEW = HOME_VIEW;
+    public static final String SECONDARY_VIEW = "Secondary View";
+    
+    @Override
+    public void init() {
+        addViewFactory(PRIMARY_VIEW, () -> new PrimaryView().getView());
+        addViewFactory(SECONDARY_VIEW, () -> new SecondaryView().getView());
+
+        DrawerManager.buildDrawer(this);
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(Oroscopo.class.getResource("style.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(Oroscopo.class.getResourceAsStream("/icon.png")));
+    }
+}
---
+++ quarta/gluon/Oroscopo/OroscopoApp/src/main/java/com/oroscopo/views/PrimaryPresenter.java
@@ -0,0 +1,45 @@
+package com.oroscopo.views;
+
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PrimaryPresenter {
+
+    @FXML
+    private View primary;
+
+    @FXML
+    private Label label;
+    @FXML
+    private Button btnSchiacciami;
+
+    public void initialize() {
+        primary.showingProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue) {
+                AppBar appBar = MobileApplication.getInstance().getAppBar();
+                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> 
+                        MobileApplication.getInstance().getDrawer().open()));
+                appBar.setTitleText("Primary");
+                appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> 
+                        System.out.println("Search")));
+            }
+        });
+    }
+    
+    @FXML
+    void buttonClick() {
+        label.setText("Hello world!");
+    }
+
+    @FXML
+    private void btnSchiacciamiClick(ActionEvent event) {
+       label.setText("Nuovo bottone schiacciato!");
+    }
+    
+}
