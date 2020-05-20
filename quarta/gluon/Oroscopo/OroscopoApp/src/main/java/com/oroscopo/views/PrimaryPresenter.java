a3d43491b75bf5ed095d709d8b79f4b7c77898f9
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-05-20 17:37:29 +0200

    Aggiorna l'interfaccia dell'esempio dell'oroscopo

--- quarta/gluon/Oroscopo/OroscopoApp/src/main/java/com/oroscopo/views/PrimaryPresenter.java
+++ quarta/gluon/Oroscopo/OroscopoApp/src/main/java/com/oroscopo/views/PrimaryPresenter.java
@@ -7,6 +7,7 @@ import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 
 public class PrimaryPresenter {
@@ -14,10 +15,17 @@ public class PrimaryPresenter {
     @FXML
     private View primary;
 
+    
     @FXML
-    private Label label;
+    private Label lblDataNascita;
     @FXML
-    private Button btnSchiacciami;
+    private Button btnOroscopo;
+    @FXML
+    private DatePicker dteDataNascita;
+    @FXML
+    private Label lblSegno;
+    @FXML
+    private Label lblOroscopo;
 
     public void initialize() {
         primary.showingProperty().addListener((obs, oldValue, newValue) -> {
@@ -32,14 +40,11 @@ public class PrimaryPresenter {
         });
     }
     
+    
     @FXML
-    void buttonClick() {
-        label.setText("Hello world!");
-    }
-
-    @FXML
-    private void btnSchiacciamiClick(ActionEvent event) {
-       label.setText("Nuovo bottone schiacciato!");
+    private void btnOroscopoClick(ActionEvent event) {
+        lblSegno.setText(GestoreOroscopo.getSegno(null));
+        lblOroscopo.setText(GestoreOroscopo.getOroscopo());
     }
     
 }
